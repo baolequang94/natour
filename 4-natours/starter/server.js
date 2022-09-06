26ad5d09553bfb76e37137f4c95132845eb4ee30
Bao Le <quangbao171@gmail.com>
2022-09-06 22:42:50 +0700

    Add mongoose and connect DB

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,7 +1,17 @@
-const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config({
+  path: './config.env',
+});
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then((con) => {
+  console.log('DB connection is successful');
+});
 
 const port = process.env.PORT || 3000;
 
