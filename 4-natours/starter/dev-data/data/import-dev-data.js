797e6d430b5c3da2c2d70286f49ab9b969d35ee6
Bao Le <quangbao1712@gmail.com>
2022-09-11 23:35:58 +0700

    Create dev fn to import mock data

---
+++ 4-natours/starter/dev-data/data/import-dev-data.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+require('dotenv').config({
+  path: './config.env',
+});
+
+const Tour = require('../../models/tourModel');
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then(() => {
+  console.log('DB connection is successful');
+});
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+console.log(tours);
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Import data successfully');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Delete data successfully');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
